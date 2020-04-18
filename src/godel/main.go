f49f5a720a364184d38031644e7b61013de22ad3
Kaden Thomas <thomas.kaden4@outlook.com>
2020-04-17 21:36:09 -0700

    Change structure

--- src/godel/main.go
+++ src/godel/main.go
@@ -1,10 +1,16 @@
 package main
 
+func (p *Program) helloWorld() {
+	for _, x := range "Hello, World\n" {
+		p.Put(100, uint16(x))
+		p.Cout(101)
+	}
+}
+
 func main() {
 	vm := CreateDebug()
 	program := Program{}
-	program.Put(100, uint16('H'))
-	program.Cout(101)
+	program.helloWorld()
 	copy(vm.memory[:], program.Bytes)
 	vm.Quiet()
 	Run(vm)
